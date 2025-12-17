728124d10d6ff798fa0ccd59241d48194cf30f2b
marco-vidali <marcovidali@outlook.com>
2025-12-17 10:02:59 +0100

    Add level complete screen

---
+++ scripts/level_complete.lua
@@ -0,0 +1,16 @@
+function show_level_complete()
+    game.update = update_level_complete
+    game.draw = draw_level_complete
+end
+
+function update_level_complete()
+    if btnp(4) then
+        -- next level
+    end
+end
+
+function draw_level_complete()
+    cls()
+    print("level complete", 36, 50, 7)
+    print("press 🅾️ to continue", 24, 70, 7)
+end
